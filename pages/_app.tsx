f6637faa4eacf53b033431162d0831f64ed4321a
Kartik <slokh@octane.gg>
2022-08-22 21:27:59 -0400

    Initialize template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from "next/app";
+import Head from "next/head";
+import "@fontsource/aboreto";
+import "@rainbow-me/rainbowkit/styles.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import theme from "../theme";
 
-export default MyApp
+const { chains, provider } = configureChains(
+  [chain.rinkeby],
+  [publicProvider()]
+);
+const { connectors } = getDefaultWallets({
+  appName: "Conditionals",
+  chains,
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Head>
+        <title>Conditionals</title>
+      </Head>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </ChakraProvider>
+  );
+};
+
+export default App;
---
+++ theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    body: "Aboreto",
+  },
+});
+
+export default theme;
